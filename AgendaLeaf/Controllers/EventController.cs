3ceba5bf6ac943d4ced0330656c49d34bb531668
Leonardo Motta <leonardomotta@ucl.br>
2023-05-10 11:42:45 -0300

    update Create Event

--- AgendaLeaf/Controllers/EventController.cs
+++ AgendaLeaf/Controllers/EventController.cs
@@ -1,7 +1,11 @@
 ﻿using AgendaLeaf.Data;
 using AgendaLeaf.Models;
+using AgendaLeaf.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Security.Claims;
 
 namespace AgendaLeaf.Controllers
@@ -23,30 +27,53 @@ namespace AgendaLeaf.Controllers
                 var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == emailClaim.Value);
                 var userid = user?.Id.ToString().ToUpper();
                 ViewBag.OwnerId = userid;
-                System.Diagnostics.Debug.WriteLine($"USERID: {userid}");
+                //System.Diagnostics.Debug.WriteLine($"USERID: {userid}");
+                ViewBag.Users = new MultiSelectList(_context.Users, "Id", "Name");
             }
             return View();
         }
 
         [HttpPost]
-        public async Task<ActionResult> Create(Event ev)
+        public async Task<ActionResult> Create([Bind("Event,UsersId")] EventViewModel eventViewModel)
         {
-            if (ev != null)
+            try
             {
-                try
+                var newEvent = new Event
                 {
-                    System.Diagnostics.Debug.WriteLine($"EVENT USERID: {ev.OwnerId} \n\n\n");
-                    System.Diagnostics.Debug.WriteLine($"EVENT NAME: {ev.Name} \n\n\n");
-                    _context.Events.Add(ev);
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction("Index", "Home");
-                }
-                catch (Exception ex)
+                    Id = Guid.NewGuid(),
+                    Name = eventViewModel.Event.Name,
+                    Description = eventViewModel.Event.Description,
+                    Date = eventViewModel.Event.Date,
+                    Type = eventViewModel.Event.Type,
+                    OwnerId = eventViewModel.Event.OwnerId
+                };
+                _context.Events.Add(newEvent);
+
+                foreach (var userId in eventViewModel.UsersId)
                 {
-                    ViewData["ErrorMessage"] = "Erro ao salvar";
-                    System.Diagnostics.Debug.WriteLine($"exception: {ex}");
+                    var Upper = userId.ToString().ToUpper();
+
+                    var newEventParticipant = new EventParticipant
+                    {
+                        Id = Guid.NewGuid(),
+                        EventId = newEvent.Id,
+                        UserId = userId
+                    };
+
+                    System.Diagnostics.Debug.WriteLine($"{newEventParticipant.Id} + {newEventParticipant.EventId} + {newEventParticipant.UserId}");
+                    _context.EventParticipants.Add(newEventParticipant);
                 }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
             }
+            catch (Exception ex)
+            {
+                ViewData["ErrorMessage"] = "Erro ao salvar";
+                System.Diagnostics.Debug.WriteLine($"exception: {ex}");
+            }
+                
+            ViewData["ErrorMessage"] = "Erro";
             return View();
         }
     }
---
+++ AgendaLeaf/Models/ViewModels/EventViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AgendaLeaf.Models.ViewModels
+{
+    public class EventViewModel
+    {
+        public Event Event { get; set; }
+        public ICollection<Guid> UsersId { get; set; }
+    }
+}
